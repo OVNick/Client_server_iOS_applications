4c9943b52293597c7f2dd8b91d3dd2025d0ddb2a
OVNick <onnickvas@gmail.com>
2022-04-20 22:17:01 +0300

    Created a project

---
+++ newVK/newVK/Controllers/LoginController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginController.swift
+//  newVK
+//
+//  Created by Николай Онучин on 20.04.2022.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
